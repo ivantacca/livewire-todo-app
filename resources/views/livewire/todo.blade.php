364db9007c5d5530fbaaab2c0c3edad47e07c654
Ivan Taccadoli <ivantaccadoli@emisfera.it>
2023-01-13 17:12:15 +0100

    Updated conditional render

--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -27,31 +27,32 @@
                 <h2 class="text-xl">Next Up</h2>
                 <span class="text-blue-700 px-2 rounded-xl bg-blue-200 ml-2">{{count($upcoming)}}</span>
             </div>
-        @endif
 
-        <div class="flex flex-col mb-5">
-            @foreach ($upcoming as $todo)
-                <div class="px-7 py-2.5 text-xl border rounded-xl mb-2.5 flex items-center">
-                    <button class="h-5 w-5 border-2 border-blue-600 mr-3 rounded" wire:click="markAsDone('{{$todo['id']}}')"></button>
-                    {{ $todo['title'] }}
-                </div>
-            @endforeach
-        </div>
+            <div class="flex flex-col mb-5">
+                @foreach ($upcoming as $todo)
+                    <div class="px-7 py-2.5 text-lg border rounded-xl mb-2.5 flex items-center">
+                        <button class="h-5 w-5 border-2 border-blue-600 mr-3 rounded" wire:click="markAsDone('{{$todo['id']}}')"></button>
+                        {{ $todo['title'] }}
+                    </div>
+                @endforeach
+            </div>
+        @endif
 
         @if(count($done) > 0)
             <div class="flex items-center mb-3.5">
                 <h2 class="text-xl">Done</h2>
                 <span class="text-blue-700 px-2 rounded-xl bg-blue-200 ml-2">{{count($done)}}</span>
             </div>
+
+            <div class=" flex flex-col">
+                @foreach ($done as $todo)
+                    <div class="px-7 py-2.5 text-lg border rounded-xl mb-2.5 flex items-center">
+                        <button class="h-5 w-5 border-2 border-blue-600 bg-blue-600 mr-3 rounded" wire:click="markAsToDo('{{$todo['id']}}')"></button>
+                        {{ $todo['title'] }}
+                    </div>
+                @endforeach
+            </div>
         @endif
 
-        <div class=" flex flex-col">
-            @foreach ($done as $todo)
-                <div class="px-7 py-2.5 text-xl border rounded-xl mb-2.5 flex items-center">
-                    <button class="h-5 w-5 border-2 border-blue-600 bg-blue-600 mr-3 rounded" wire:click="markAsToDo('{{$todo['id']}}')"></button>
-                    {{ $todo['title'] }}
-                </div>
-            @endforeach
-        </div>
     </div>
 </div>
